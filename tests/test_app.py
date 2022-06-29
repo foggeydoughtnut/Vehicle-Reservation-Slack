66c080428aa7476f3f58307ab79de91de8e4e90a
Jeff <foggeydoughtnut@gmail.com>
2022-06-29 08:35:38 -0600

    separated the db logic tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,4 @@
-from app import create_data_dict, app as testApp, create_admin_user, load_user
-from models import User, db
+from app import create_data_dict, app as testApp
 import pytest
 
 @pytest.fixture()
@@ -30,41 +29,3 @@ def test_create_data_dict():
     error_data = create_data_dict(invalid_input_data)
     assert type(data) == dict
     assert error_data["Error"]
-
-def test_user_loader():
-    # Create User to get
-    testUser = User('test-user')
-    testUser.set_password('test')
-    testUser.password_hash
-    db.session.add(testUser)
-    db.session.commit()
-
-    # Get the User
-    test = load_user(testUser.id)
-    assert test.username == 'test-user'
-    assert test.check_password('test')
-
-    # Delete the User
-    User.query.filter_by(id=test.id).delete()
-    db.session.commit() 
-
-# def test_create_admin_user():
-#     # Delete admin user if it exists
-#     old_admin_user = session.query(User).filter(User.username == 'admin')
-#     print(old_admin_user)
-#     # User.query.filter(User.username == 'admin').delete()
-#     # db.session.commit()
-#     # Create the admin user
-#     create_admin_user()
-#     # Check that the admin user is in the database
-#     admin = User.query.filter(User.username == 'admin')
-#     assert admin.username == 'admin'
-#     assert admin.check_password('test')
-
-# def test_logout_redirect(client):
-#     response = client.get("/logout")
-#     print(response.status)
-#     print(response.request.path)
-#     # assert len(response.history) == 1
-#     # assert response.request.path == "/login"
-
